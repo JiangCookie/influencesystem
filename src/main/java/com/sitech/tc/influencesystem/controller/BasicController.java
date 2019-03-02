1619eae50ac5249ccc8c5723eb0ba153a2fd687d
Jiang <335131226@qq.com>
2019-03-02 10:12:32 +0800

    需求文档，PPT

--- src/main/java/com/sitech/tc/influencesystem/controller/BasicController.java
+++ src/main/java/com/sitech/tc/influencesystem/controller/BasicController.java
@@ -1,8 +1,13 @@
 package com.sitech.tc.influencesystem.controller;
 
+import com.sitech.tc.influencesystem.common.CookieUtil;
+import com.sitech.tc.influencesystem.common.RedisOperator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author JYH
@@ -10,6 +15,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class BasicController {
+
+    @Autowired
+    public RedisOperator redis;
+
     @GetMapping("/index")
     public String index(){
         return "index1";
@@ -17,12 +26,36 @@ public class BasicController {
 
 
     @GetMapping("/form_basic")
-    public String form_basic(){
+    public String form_basic(HttpServletRequest request){
+        //检查是否登录
+        String redisValue = isLogin(request);
+        if(redisValue == null){
+            return  "redirect:/index";
+        }
         return "form_basic";
     }
 
     @GetMapping("/newform")
-    public String newform(){
+    public String newform(HttpServletRequest request){
+        //检查是否登录
+        String redisValue = isLogin(request);
+        if(redisValue == null){
+            return  "redirect:/index";
+        }
         return "newform1";
+    }
+
+
+    /**
+     * @Description: 判断是否登录
+     */
+    public String isLogin(HttpServletRequest request){
+        String loginToken = CookieUtil.readLoginToken(request);
+        String redisValue = null;
+        if(loginToken != null){
+            redisValue = redis.get(loginToken);
+        }
+
+        return redisValue;
     }
 }
--- src/main/resources/static/js/form.js
+++ src/main/resources/static/js/form.js
@@ -96,7 +96,8 @@ layui.use('table', function () {
                 }
             });
         }
-
+        location.reload();
     });
+
 });
 
